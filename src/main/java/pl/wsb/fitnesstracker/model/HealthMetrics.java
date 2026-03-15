c0696ee6ee6f97510a5341e62fb97b9e6c837b12
vdi-terminal <jakubsiek1234@gmail.com>
2026-03-15 17:21:04 +0100

    zad. pkt.11 - fixed

--- src/main/java/pl/wsb/fitnesstracker/model/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/model/HealthMetrics.java
@@ -1,13 +1,11 @@
 package pl.wsb.fitnesstracker.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "health_metrics") // Jawna nazwa tabeli
+@Table(name = "health_metrics")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
@@ -17,11 +15,27 @@ public class HealthMetrics {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "weight", nullable = false)
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "weight")
     private int weight;
 
-    // Konstruktor dla Ciebie, żebyś mógł łatwo tworzyć obiekty
-    public HealthMetrics(int weight) {
+    @Column(name = "height")
+    private int height;
+
+    @Column(name = "heart_rate")
+    private int heartRate;
+
+
+    public HealthMetrics(int userId, LocalDate date, int weight, int height, int heartRate) {
+        this.userId = userId;
+        this.date = date;
         this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
     }
 }
