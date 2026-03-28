4025583ba18f9a98c1508961dfeabc6bb9169146
grafic121 <michalm.maciej@gmail.com>
2026-03-28 12:04:55 +0100

    x

--- src/main/java/pl/wsb/fitnesstracker/statistics/internal/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/internal/Statistics.java
@@ -1,31 +1,33 @@
 package pl.wsb.fitnesstracker.statistics.internal;
 
-import jakarta.annotation.Nullable;
+// KLUCZOWY IMPORT: Importujemy Usera z jego własnego modułu
+import pl.wsb.fitnesstracker.user.internal.User;
+
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "Statistics")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+@NoArgsConstructor
 public class Statistics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Nullable
     private Long id;
 
+    // Relacja OneToOne do klasy User z innego pakietu
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
     private int totalTrainings;
-
     private double totalDistance;
+    private double totalCaloriesBurned;
 
-    private int totalCaloriesBurned;
-
-    public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
+    public Statistics(final User user, final int totalTrainings, final double totalDistance, final double totalCaloriesBurned) {
+        this.user = user;
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
