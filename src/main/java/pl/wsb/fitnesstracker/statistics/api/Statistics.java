d78c7f4ebb40d10f6a9c10103bde7cc8038b68cc
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-03-28 15:59:28 +0100

    entities update

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
 @Table(name = "Statistics")
@@ -19,10 +20,17 @@ public class Statistics {
     @Nullable
     private Long id;
 
+    @OneToOne
+    @JoinColumn(name = "user_Id")
+    private User user;
+
+    @Column(name = "Total_Trainings", nullable = false)
     private int totalTrainings;
 
+    @Column(name = "Total_Distance", nullable = false)
     private double totalDistance;
 
+    @Column(name = "Total_Calories_Burned", nullable = false)
     private int totalCaloriesBurned;
 
     public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,26 +1,40 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
 import lombok.Getter;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
 
 import java.util.Date;
 
+@Entity
+@Table(name = "Training")
 @Getter
+
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_Id")
     private User user;
 
+    @Column(name = "Start_Time", nullable = false)
     private Date startTime;
 
+    @Column(name = "End_Time", nullable = false)
     private Date endTime;
 
+    @Column(name = "Activity_Type", nullable = false)
     private ActivityType activityType;
 
+    @Column(name = "Distance", nullable = false)
     private double distance;
 
+    @Column(name = "Average_Speed", nullable = false)
     private double averageSpeed;
 
     public Training(
