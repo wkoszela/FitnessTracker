4d601a35a2c3d3e6f7bc48aab0d4f799e0f2d66f
maestro <kuchalskimariusz@gmail.com>
2026-03-29 15:07:42 +0200

    Tabela Trainings

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,5 +1,6 @@
 package pl.wsb.fitnesstracker.training.api;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
@@ -7,20 +8,31 @@ import pl.wsb.fitnesstracker.user.api.User;
 import java.util.Date;
 
 @Getter
+@Entity
+@Table(name = "Trainings")
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "startTime", nullable = false)
     private Date startTime;
 
+    @Column(name = "endTime", nullable = false)
     private Date endTime;
 
+    @Column(name = "activityType", nullable = false)
     private ActivityType activityType;
 
+    @Column(name = "distance", nullable = false)
     private double distance;
 
+    @Column(name = "averageSpeed", nullable = false)
     private double averageSpeed;
 
     public Training(
