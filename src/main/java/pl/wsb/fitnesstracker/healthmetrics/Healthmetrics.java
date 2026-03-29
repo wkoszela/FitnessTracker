2146fb71547979918b7bd15c631573c735a2f751
patnap19 <patryk.napiorkowski95@gmail.com>
2026-03-29 16:19:04 +0200

    dodanie pliku github-ci-cd.yml do .github/workflows

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/Healthmetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/Healthmetrics.java
@@ -7,48 +7,44 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.time.LocalDate;
 @Entity
-@Table(name = "healthmetrics")
+@Table(name = "healthMetrics")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
-
-
-public class Healthmetrics {
-
+public class HealthMetrics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Nullable
     private Long id;
 
-    @Column(name="user_id",nullable = false)
-    private String userId;
+    @Column(name = "user_id", nullable = false)
+    private int userID;
 
-    @Column(name="date",nullable = false)
-    private String date;
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
 
-    @Column(name="weight",nullable = false)
-    private float weight;
+    @Column(name = "weight", nullable = false)
+    private double weight;
 
-    @Column(name="height",nullable = false)
-    private float height;
+    @Column(name = "height", nullable = false)
+    private double height;
 
-    @Column(name="heartRate",nullable = false)
-    private float heartRate;
+    @Column(name = "hearthRate", nullable = false)
+    private int hearthRate;
 
-    public Healthmetrics(
-            String userId,
-            String date,
-            float weight,
-            float height,
-            float heartRate) {
-        this.userId = userId;
-        this.date = date;
-        this.weight = weight;
-        this.height = height;
-        this.heartRate = heartRate;
+    public HealthMetrics(
+            final int userID,
+            final LocalDate date,
+            final double weight,
+            final double height,
+            final int hearthRate){
+                this.userID = userID;
+                this.date = date;
+                this.weight = weight;
+                this.height = height;
+                this.hearthRate = hearthRate;
     }
-
-
 
 }
