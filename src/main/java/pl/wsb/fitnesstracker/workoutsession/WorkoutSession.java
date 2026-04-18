5834275b359f5353ccfd1c068a9057b92086e027
vdi-terminal <anna-czarnecka@outlook.com>
2026-04-18 10:21:57 +0200

    added 3 entites

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,63 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import java.time.LocalDate;
+
+
+
+@Entity
+@Table(name = "workoutsession")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
     private String timestamp;
+
+    @Column(nullable = false)
     private double startLatitude;
+
+    @Column(nullable = false)
     private double startLongitude;
+
+    @Column(nullable = false)
     private double endLatitude;
+
+    @Column(nullable = false)
     private double endLongitude;
+
+    @Column(nullable = false)
     private double altitude;
-}
+
+    public WorkoutSession(User user, String timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude) {
+
+        this.user = user;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
+    }
+
+
+
+
