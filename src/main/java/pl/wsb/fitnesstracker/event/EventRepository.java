f5d74196b3482e492b831e96e194c85b85221644
artem <tlpzk777@gmail.com>
2026-04-19 12:33:56 +0200

    added interface for querying

---
+++ src/main/java/pl/wsb/fitnesstracker/event/EventRepository.java
@@ -0,0 +1,12 @@
+package pl.wsb.fitnesstracker.event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("SELECT e FROM Event e WHERE e.startTime > :now ORDER BY e.startTime")
+    List<Event> findUpcoming(@Param("now") LocalDate now);
+}
